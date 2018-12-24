05104ebf7d78a0c34e0e507d7d0f43b2d572d2f7
Ryan Curry <46105013+ryancurry1776@users.noreply.github.com>
2018-12-24 23:10:49 +0400

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,3 @@
-(function($) {
-
-	"use strict";
-
-	/* --------------------------------------------------- */
-	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      }); 
-  	})
-})(jQuery);
+$(document).ready(function() {
+  $('#preloader').delay(300).fadeOut(1000);
+});
